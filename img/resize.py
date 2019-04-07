f02810d80775b7d5fcb588ecfd6b77d7b6e80997
Shalev <lifshitz.shalev@gmail.com>
2019-04-07 13:10:44 -0400

    Added Feb & march Newsletter

--- img/resize.py
+++ img/resize.py
@@ -1,10 +1,16 @@
 import cv2
 import matplotlib.pyplot as plt
 
-def resize(img_file, save_name):
+def resize_portfolio(img_file, save_name):
     raw = cv2.imread(img_file)
     new = cv2.resize(raw, (int(raw.shape[0]*(900/712)), raw.shape[0]))
     plt.imshow(new)
     cv2.imwrite(save_name, new)
     
-resize("sxsw_talk2.png", "sxsw_portfolio.png")
+def resize_newsetter(img_file, save_name):
+    raw = cv2.imread(img_file)
+    new = cv2.resize(raw, (int(raw.shape[0]*(1080/720)), raw.shape[0]))
+    plt.imshow(new)
+    cv2.imwrite(save_name, new)
+    
+resize_newsetter("sxsw_talk3.png", "sxsw_talk_newsletter.png")
