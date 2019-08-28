d2351d244a937aa53c63969893c668f0daef5d97
Shalev <lifshitz.shalev@gmail.com>
2019-08-27 21:52:04 -0400

    Added Spring Newsletter and Updated Bio

--- img/resize.py
+++ img/resize.py
@@ -6,11 +6,11 @@ def resize_portfolio(img_file, save_name):
     new = cv2.resize(raw, (int(raw.shape[0]*(900/712)), raw.shape[0]))
     plt.imshow(new)
     cv2.imwrite(save_name, new)
-    
+
 def resize_newsetter(img_file, save_name):
     raw = cv2.imread(img_file)
     new = cv2.resize(raw, (int(raw.shape[0]*(1080/720)), raw.shape[0]))
     plt.imshow(new)
     cv2.imwrite(save_name, new)
-    
-resize_newsetter("sxsw_talk3.png", "sxsw_talk_newsletter.png")
+
+resize_newsetter("meConvention_edit.png", "meConvention_newsletter.jpg")
